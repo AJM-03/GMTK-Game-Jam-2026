852440fbd4419f0782918cc3fb0f4c136e8ba357
AJM-03 <a.moreland2003@gmail.com>
2026-07-23 00:47:21 +0100

    Animal Animations

---
+++ Assets/Scripts/AnimalAnimator.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnimalAnimator : MonoBehaviour
+{
+    private Animator animator;
+
+    public enum Anim
+    {
+        Attack,
+        Bounce,
+        Clicked,
+        Death,
+        Eat,
+        Fear,
+        Fly,
+        Hit,
+        Idle_A, 
+        Idle_B, Idle_C,
+        Jump,
+        Roll,
+        Lay,
+        Run,
+        Sit,
+        Spin,
+        Swim,
+        Walk
+    }
+
+    public enum Emotion
+    {      
+        Eyes_Annoyed,
+        Eyes_Blink,
+        Eyes_Cry,
+        Eyes_Dead,
+        Eyes_Excited,
+        Eyes_Happy,
+        Eyes_LookDown,
+        Eyes_LookIn,
+        Eyes_LookOut,
+        Eyes_LookUp,
+        Eyes_Rabid,
+        Eyes_Sad,
+        Eyes_Shrink,
+        Eyes_Sleep,
+        Eyes_Spin,
+        Eyes_Squint,
+        Eyes_Trauma,
+        Sweat_L,
+        Sweat_R,
+        Teardrop_L,
+        Teardrop_R
+    };
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        ChangeAnimation(Anim.Idle_A);
+        ChangeShapekey(Emotion.Eyes_Blink);  // Start by blinking
+    }
+
+
+    public void ChangeAnimation(Anim a)
+    {
+        // If Spin/Splash animation
+        if ((int)a == 16)
+        {
+            if (animator.HasState(0, Animator.StringToHash("Spin")))
+            {
+                animator.Play("Spin");
+                // dropdownAnimation.options[index] = new Dropdown.OptionData("Spin");
+            }
+            else if (animator.HasState(0, Animator.StringToHash("Splash")))
+            {
+                animator.Play("Splash");
+                // dropdownAnimation.options[index] = new Dropdown.OptionData("Splash");
+            }
+        }
+        else
+        {
+            animator.Play(a.ToString());
+        }
+    }
+
+    public void ChangeShapekey(Emotion e)
+    {
+        animator.Play(e.ToString());
+    }
+}
