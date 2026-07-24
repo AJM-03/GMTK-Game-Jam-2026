9020fd0ba05bb56c19317dee85096ec4e2120508
AJM-03 <a.moreland2003@gmail.com>
2026-07-24 15:38:26 +0100

    Environment Base

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -55,7 +55,7 @@ public class GameController : MonoBehaviour
     private void SpawnAnimal()
     {
         GameObject newAnimal = Instantiate(animalPrefabs[Random.Range(0, animalPrefabs.Count)],
-                                           new Vector3(Random.Range(-15, 15), 0, Random.Range(-20, -5)),
+                                           new Vector3(Random.Range(-15, 15), 0.5f, Random.Range(-20, -5)),
                                            Quaternion.identity);
         Animal animalScript = newAnimal.GetComponent<Animal>();
         newAnimal.transform.localScale *= Random.Range(1f - animalScript.scaleVariance, 1f + animalScript.scaleVariance);
