95e1ffbbcbedf31ae3192ebea2dab676de4ad4c5
Cjm-09 <cjmoreland009@gmail.com>
2026-07-24 22:18:16 +0100

    Volume Slider saves

--- Assets/Scripts/VolumeManager.cs
+++ Assets/Scripts/VolumeManager.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VolumeManager : MonoBehaviour
+public class ValumManager1 : MonoBehaviour
 {
-    [SerializeField] Slider volumeSlider;
+    [SerializeField]
+    Slider volumeSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+        else
+        {
+            Load();
+        }
+    }
 
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
+        Save();
     }
 
-    public void Start()
+    private void Load()
     {
-        volumeSlider.value = 100f;
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
 }
