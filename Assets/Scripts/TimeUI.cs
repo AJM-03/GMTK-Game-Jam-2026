0db46118c6f17e201f0bb0a3f99208c7101b32c9
AJM-03 <a.moreland2003@gmail.com>
2026-07-25 11:51:42 +0100

    Timer Shake

--- Assets/Scripts/TimeUI.cs
+++ Assets/Scripts/TimeUI.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -10,11 +11,16 @@ public class TimeUI : MonoBehaviour
     public TMP_Text timeText;
     public Slider slider;
     private GameController gameController;
+    private bool shaking;
+    private Quaternion timerRot;
+    private RectTransform timerRect;
 
 
     private void Start()
     {
         gameController = FindObjectOfType<GameController>();
+        timerRect = timeText.GetComponent<RectTransform>();
+        timerRot = timerRect.rotation;
     }
 
     public void Update()
@@ -23,5 +29,15 @@ public class TimeUI : MonoBehaviour
 
         float t = Mathf.InverseLerp(gameController.startingTime, 0f, gameController.timer);
         slider.value = t;
+
+        timerRect.rotation = timerRot;
+
+        if (!shaking && gameController.timer <= 5) Shake();
+    }
+
+    private void Shake()
+    {
+        shaking = true;
+        slider.GetComponent<RectTransform>().DORotate(new Vector3(0, 0, 7f), 0.075f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutQuart);
     }
 }
